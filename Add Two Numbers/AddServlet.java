f79ead2b20477012aa6929342cc9dd53af4b88f8
Nitesh <54433181+Niteshtt@users.noreply.github.com>
2020-08-24 12:34:29 +0530

    Create AddServlet.java

---
+++ Add Two Numbers/AddServlet.java
@@ -0,0 +1,23 @@
+package com.nits;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Addservlet extends HttpServlet 
+	{
+		public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+		{
+			int i = Integer.parseInt(req.getParameter("num1"));
+			int j = Integer.parseInt(req.getParameter("num2"));
+			int k = i+j;
+			
+                        PrintWriter out = res.getWriter();
+                        out.println("Result is " + k);
+ 
+		}
+	}
