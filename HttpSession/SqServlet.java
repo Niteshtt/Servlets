6176ec34b8830e8cdb8e46dfd320baca5e4dc077
Nitesh <54433181+Niteshtt@users.noreply.github.com>
2020-08-26 19:12:25 +0530

    Create SqServlet.java

---
+++ HttpSession/SqServlet.java
@@ -0,0 +1,21 @@
+package com.httpsession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SqServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+    	HttpSession session = req.getSession();
+    	int k = (int) session.getAttribute("k");
+      	 k=k*k;
+      	 PrintWriter out = res.getWriter();
+            out.println("Result is " + k);
+       	System.out.println("sql");
+       }
+   }
+
